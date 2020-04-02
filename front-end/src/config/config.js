b6da31825fe0fed9f599b968381d6b7253a6af1b
harman <harman.sarwara@gmail.com>
2020-04-01 22:10:36 -0400

    commit kardiya1

--- front-end/src/config/config.js
+++ front-end/src/config/config.js
@@ -1,3 +1,14 @@
-module.exports = {
-    baseUrl: 'http://localhost:5000/api/v1/'
+import pkg from '../package.json';
+
+export default {
+  development: {
+    endpoint: pkg.proxy
+  },
+  production: {
+    endpoint: window.location.hostname
+  }
 }
+
+// module.exports = {
+//     baseUrl: 'http://localhost:5000/api/v1/'
+// }
