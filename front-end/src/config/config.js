70d662b6d36f919f270733b02eb6ae46513c82aa
harman <harman.sarwara@gmail.com>
2020-04-01 22:14:57 -0400

    commit kardiya1

--- front-end/src/config/config.js
+++ front-end/src/config/config.js
@@ -1,14 +1,14 @@
-import pkg from '../package.json';
+// import pkg from '../package.json';
 
-export default {
-  development: {
-    endpoint: pkg.proxy
-  },
-  production: {
-    endpoint: window.location.hostname
-  }
-}
-
-// module.exports = {
-//     baseUrl: 'http://localhost:5000/api/v1/'
+// export default {
+//   development: {
+//     endpoint: pkg.proxy
+//   },
+//   production: {
+//     endpoint: window.location.hostname
+//   }
 // }
+
+module.exports = {
+    baseUrl: 'https://mern-crud21.herokuapp.com/api/v1/'
+}
