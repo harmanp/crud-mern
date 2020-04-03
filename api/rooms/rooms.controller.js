d98cbf3f122df6868754d589f939487ed702dc88
harman <harman.sarwara@gmail.com>
2020-04-02 20:08:43 -0400

    commiting status fix

--- api/rooms/rooms.controller.js
+++ api/rooms/rooms.controller.js
@@ -44,8 +44,7 @@ res.status(200).json({ message: "room Details fetched Successfully", data : room
        },
        delete: (req, res)=>{
             const roomId = req.params.id;
-            Room
-            .findByIdAndUpdate(roomId)
+            Room.findByIdAndUpdate(roomId)
             .exec((err, roomDetails) => {
                  if (err) res.status(500).json({message : err})
                  res.status(200).json({ message: "room Deleted"});
